bf101117d8c234c7f3734638ad185e8c257ca679
Matt Walston <m@mlw.ac>
2013-11-17 12:19:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Motherlode::Application.config.secret_key_base = '638dbf10d1a3a0ad60b67e1c7584f7d00cea2c91e9400c570e192b183abd3d9570a068f0d3ca409a38efa06a23764ef0bd3d9c6322cd21f67f14c0d26b499c51'
