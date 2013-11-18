458ee6be8a4a45fa3f9d6f84b6ee6ac99ec72dca
Matt Walston <m@mlw.ac>
2013-11-18 16:58:27 -0500

    Create user if not logged in with current_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user = User.new if @current_user.nil?
   end
 
   def current_product
